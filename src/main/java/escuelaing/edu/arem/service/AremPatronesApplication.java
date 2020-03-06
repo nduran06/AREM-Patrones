d3d540fd1d3edead51e1126a2aee2b15f6728871
nduran06 <natalia.duran@mail.escuelaing.edu.co>
2020-03-06 12:49:17 -0500

    first commit

---
+++ src/main/java/escuelaing/edu/arem/service/AremPatronesApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.arem.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AremPatronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AremPatronesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/escuelaing/edu/arem/service/AremPatronesApplicationTests.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.arem.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AremPatronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
